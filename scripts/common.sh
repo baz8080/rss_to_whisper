7528151f795a6e3d253068bb857d844ecff11fda
Barry Carroll <itsmrbarry@gmail.com>
2024-04-06 13:48:47 +0100

    Check for SITE_DIR and SOURCE_DIR before continuing in scripts

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 export SITE_DIR="$HOME/Code/podscripts_site"
+export SOURCE_DIR="/Volumes/rss_to_whisper"
+
 export POSTS_DIR="$SITE_DIR/_posts"
-export SOURCE_DIR="$HOME/rss_to_whisper"
+
 export PODS_DIR="$SOURCE_DIR/pods"
 export BUILD_DIR="$SOURCE_DIR/build"
+
+set +e
+
+if [ -z "$SITE_DIR" ] || [ -z "$SOURCE_DIR" ]; then
+    echo "SITE_DIR and / or SOURCE_DIR variables are missing. Exiting"
+    exit 1
+fi
