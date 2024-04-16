6fa08ac904fa2117f6dcafb408b3dd43d2d3e9b4
Barry Carroll <itsmrbarry@gmail.com>
2024-04-16 08:33:29 +0100

    Added collections for different broad themes of podcasts

--- rss_to_whisper.py
+++ rss_to_whisper.py
@@ -1,9 +1,7 @@
 import argparse
-from dotenv import load_dotenv
 import logging
 import os
 import re
-import sys
 import time
 from collections import namedtuple
 from pathlib import Path
@@ -12,33 +10,25 @@ import feedparser
 import requests
 import torch
 import whisper
+from dotenv import load_dotenv
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
 from whisper.utils import WriteTXT, WriteTSV
 
-from utils import is_writable, time_to_seconds, get_hash, get_file_part, default_feeds, create_path, chunk
+import utils
+from utils import is_writable, get_file_part, default_feeds, create_path, chunk, initialise_logging, get_episode_dict
 
 logger = logging.getLogger(__name__)
 load_dotenv()
 
 
-def main(data_dir: str, feed_uri: str, verbose: bool, model_name: str, process_local_only: bool):
-    initialise_logging(verbose)
-    process_feeds(data_dir, feed_uri, model_name, process_local_only)
+def main(data_dir: str, single_feed_uri: str, single_feed_collection: str,
+         verbose: bool, model_name: str, process_local_only: bool):
+    initialise_logging(logger, verbose)
+    initialise_logging(utils.logger, verbose)
 
-
-def initialise_logging(verbose: bool):
-    handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-
-    if verbose:
-        logger.level = logging.DEBUG
-        handler.setLevel(logging.DEBUG)
-    else:
-        logger.level = logging.INFO
-        handler.setLevel(logging.INFO)
+    process_feeds(data_dir=data_dir, single_feed_uri=single_feed_uri, single_feed_collection=single_feed_collection,
+                  model_name=model_name, process_local_only=process_local_only)
 
 
 def initialise_whisper(model_name: str):
@@ -48,85 +38,91 @@ def initialise_whisper(model_name: str):
     return model
 
 
-def process_feeds(data_dir: str, feed_uri: str, model_name: str, process_local_only: bool = False):
+def process_feeds(data_dir: str, single_feed_uri: str, single_feed_collection: str,
+                  model_name: str, process_local_only: bool = False):
     if data_dir is None or not is_writable(data_dir):
         logger.error("The data_dir is missing, or not writable. Cannot continue")
         exit(1)
 
-    if feed_uri is None:
-        logger.info("Processing default feeds")
-        feed_uris = default_feeds()
+    if single_feed_uri and single_feed_collection:
+        logger.info(f"Processing single feed {single_feed_uri}")
+        feed_collections = {
+            single_feed_collection.lower(): [single_feed_uri]
+        }
     else:
-        logger.info(f"Processing single feed {feed_uri}")
-        feed_uris = [feed_uri]
+        logger.info("Processing default feeds")
+        feed_collections = default_feeds()
 
     episode_dicts = []
     whisper_model = None
 
-    for feed_uri in feed_uris:
-        feed_response = get_feed(feed_uri)
-        logger.info(f"Processing {feed_uri}")
+    for collection in feed_collections.keys():
+        for feed_uri in feed_collections[collection]:
+            feed_response = get_feed(feed_uri)
+            logger.info(f"Processing {feed_uri}")
 
-        if feed_response and feed_response.feed:
-            pod_path = create_path(data_dir, feed_response.feed.title)
+            if feed_response and feed_response.feed:
+                pod_path = create_path(data_dir, feed_response.feed.title)
 
-            if not pod_path:
-                logger.error("Cannot find podcast title")
-                return
+                if not pod_path:
+                    logger.error("Cannot find podcast title")
+                    return
 
-            for entry in feed_response.entries:
-                try:
-                    entry_title_and_date = get_episode_title_with_date(entry)
-                    episode_directory_path = create_path(pod_path, entry_title_and_date)
+                for entry in feed_response.entries:
+                    try:
+                        entry_title_and_date = get_episode_title_with_date(entry)
+                        episode_directory_path = create_path(pod_path, entry_title_and_date)
 
-                    if not episode_directory_path:
-                        logger.error("Failed to make directory for the episode")
-                        continue
+                        if not episode_directory_path:
+                            logger.error("Failed to make directory for the episode")
+                            continue
 
-                    mp3_info = get_mp3_info(entry.links, episode_directory_path)
+                        mp3_info = get_mp3_info(entry.links, episode_directory_path)
 
-                    if mp3_info is None:
-                        logger.warning(f"{entry.title} has no mp3 link. Skipping")
-                        continue
+                        if mp3_info is None:
+                            logger.warning(f"{entry.title} has no mp3 link. Skipping")
+                            continue
 
-                    mp3_and_transcript_exist = (mp3_info.file_path.exists() and
-                                                (episode_directory_path / "transcribed").exists())
+                        mp3_and_transcript_exist = (mp3_info.file_path.exists() and
+                                                    (episode_directory_path / "transcribed").exists())
 
-                    if mp3_and_transcript_exist:
-                        transcript_text = get_transcript_text(episode_directory_path / f"{mp3_info.file_name}.txt")
-                        episode_dicts.append(get_episode_dict(feed_response.feed, entry, transcript_text))
-                    elif process_local_only:
-                        continue
-                    else:
-                        download_file_if_required(mp3_info)
+                        if mp3_and_transcript_exist:
+                            transcript_text = get_transcript_text(episode_directory_path / f"{mp3_info.file_name}.txt")
+                            episode_dicts.append(
+                                get_episode_dict(feed_response.feed, entry, transcript_text, collection))
+                        elif process_local_only:
+                            continue
+                        else:
+                            download_file_if_required(mp3_info)
 
-                        if whisper_model is None:
-                            whisper_model = initialise_whisper(model_name)
+                            if whisper_model is None:
+                                whisper_model = initialise_whisper(model_name)
 
-                        transcribe_if_required(whisper_model, mp3_info, episode_directory_path)
-                        transcript_text = get_transcript_text(episode_directory_path / f"{mp3_info.file_name}.txt")
-                        episode_dicts.append(get_episode_dict(feed_response.feed, entry, transcript_text))
+                            transcribe_if_required(whisper_model, mp3_info, episode_directory_path)
+                            transcript_text = get_transcript_text(episode_directory_path / f"{mp3_info.file_name}.txt")
+                            episode_dicts.append(
+                                get_episode_dict(feed_response.feed, entry, transcript_text, collection))
 
-                except Exception as e:
-                    logger.error(f"Couldn't process episode entry: {entry.title}")
-                    logger.error(e)
+                    except Exception as e:
+                        logger.error(f"Couldn't process episode entry: {entry.title}")
+                        logger.error(e)
 
     elastic_api_key = os.getenv("ELASTIC_API_KEY")
     elastic_client = Elasticsearch(hosts="https://nasty.local:9200/", api_key=elastic_api_key, verify_certs=False)
-    # elastic_client.indices.delete(index="podcasts")
-    # elastic_client.indices.create(
-    #     index="podcasts",
-    #     body={
-    #         "settings": {
-    #             "index.store.preload": ["nvd", "dvd"]
-    #         }
-    #     }
-    # )
+    elastic_client.indices.delete(index="podcasts")
+    elastic_client.indices.create(
+        index="podcasts",
+        body={
+            "settings": {
+                "index.store.preload": ["nvd", "dvd"]
+            }
+        }
+    )
     bulk(client=elastic_client, actions=generate_data_for_indexing(episode_dicts))
 
 
 def generate_data_for_indexing(_episode_dicts):
-    for chunked_list in chunk(_episode_dicts, 500):
+    for chunked_list in chunk(_episode_dicts, 300):
         logger.info(f"Processing chunk of size {len(chunked_list)}")
         for episode_dict in chunked_list:
             yield episode_dict
@@ -244,99 +240,6 @@ def replace_repeated_phrases(text, threshold=13):
     return replaced_text, text_was_changed
 
 
-def get_episode_dict(podcast_metadata, episode_data, transcript: str):
-    episode_dict = None
-
-    _id = get_hash(transcript)
-
-    episode_audio_link = [d["href"] for d in episode_data.links if d["rel"] == "enclosure"]
-    if episode_audio_link and len(episode_audio_link) > 0:
-        episode_audio_link = episode_audio_link[0]
-    else:
-        logger.error(f"Skipping episode because it has no MP3")
-        return episode_dict
-
-    try:
-        podcast_title = podcast_metadata.title
-        podcast_link = getattr(podcast_metadata, "link", None)
-        podcast_language = getattr(podcast_metadata, "language", None)
-        podcast_copyright = getattr(podcast_metadata, "rights", None)
-        podcast_author = getattr(podcast_metadata, "author", None)
-
-        # todo normalise the tags, dupes, case etc
-        podcast_tags = getattr(podcast_metadata, "tags", None)
-        if podcast_tags:
-            podcast_tags = [d["term"] for d in podcast_tags]
-
-        podcast_image = getattr(podcast_metadata, "image", None)
-        if podcast_image:
-            podcast_image = podcast_image.href
-
-        podcast_type = getattr(podcast_metadata, "itunes_type", None)
-
-        episode_title = episode_data.title
-
-        episode_published_on = time.strftime('%Y-%m-%d', episode_data.published_parsed)
-        episode_web_link = getattr(episode_data, "link", None)
-
-        episode_image = getattr(episode_data, "image", None)
-        if episode_image:
-            episode_image = getattr(episode_image, "href", None)
-
-        episode_summary = getattr(episode_data, "summary", None)
-        episode_subtitle = getattr(episode_data, "subtitle", None)
-        episode_authors = getattr(episode_data, "authors", None)
-        episode_number = getattr(episode_data, "itunes_episode", None)
-        episode_season = getattr(episode_data, "itunes_season", None)
-        episode_type = getattr(episode_data, "itunes_episodetype", None)
-        episode_keywords = getattr(episode_data, "itunes_keywords", None)
-
-        episode_duration = getattr(episode_data, "itunes_duration", None)
-        if episode_duration and ":" in episode_duration:
-            episode_duration = time_to_seconds(episode_duration)
-
-        # todo maybe include podcast_transcripts
-        # todo combine tags and keywords for episode?
-        episode_tags = getattr(episode_data, "tags", None)
-        if episode_tags:
-            episode_tags = [d["term"] for d in episode_tags]
-
-        episode_dict = {
-            "_id": _id,
-            "_index": "podcasts",
-            "podcast_title": podcast_title,
-            "podcast_link": podcast_link,
-            "podcast_language": podcast_language,
-            "podcast_copyright": podcast_copyright,
-            "podcast_author": podcast_author,
-            "podcast_tags": podcast_tags,
-            "podcast_image": podcast_image,
-            "podcast_type": podcast_type,
-            "episode_title": episode_title,
-            "episode_published_on": episode_published_on,
-            "episode_audio_link": episode_audio_link,
-            "episode_web_link": episode_web_link,
-            "episode_image": episode_image,
-            "episode_summary": episode_summary,
-            "episode_subtitle": episode_subtitle,
-            "episode_authors": episode_authors,
-            "episode_number": episode_number,
-            "episode_season": episode_season,
-            "episode_type": episode_type,
-            "episode_keywords": episode_keywords,
-            "episode_duration": episode_duration,
-            "episode_tags": episode_tags,
-            "episode_transcript": transcript
-        }
-
-        return episode_dict
-
-    except AttributeError as e:
-        logger.error(f"Error getting podcast metadata")
-        logger.error(e)
-        return None
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog='rss_to_whisper.py',
@@ -346,11 +249,13 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--data-dir", required=True,
                         help="Provide a path to a writable directory where pods will be downloaded to.")
     parser.add_argument("-f", "--feed", required=False,
-                        help="Provide an rss feed, e.g. http://feeds.libsyn.com/60664 ")
+                        help="Provide a single rss feed, e.g. http://feeds.libsyn.com/60664. Must also provide a "
+                             "collection with -c --collection")
     parser.add_argument("-v", "--verbose", action='store_true')
     parser.add_argument("-m", "--model-name", required=False, default="medium")
     parser.add_argument("-l", "--local-only", required=False, action="store_true")
+    parser.add_argument("-c", "--collection", required=False)
 
     args = parser.parse_args()
-    main(data_dir=args.data_dir, feed_uri=args.feed, verbose=args.verbose,
-         model_name=args.model_name, process_local_only=args.local_only)
+    main(data_dir=args.data_dir, single_feed_uri=args.feed, single_feed_collection=args.collection,
+         verbose=args.verbose, model_name=args.model_name, process_local_only=args.local_only)
--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
 import hashlib
+import logging
 import os
 import re
+import sys
+import time
 import uuid
 from pathlib import Path
 from urllib.parse import urlparse
+
+logger = logging.getLogger(__name__)
 
 
 def is_valid_uuid(uuid_str: str):
@@ -72,28 +77,149 @@ def create_path(_parent_path, _directory_name: str):
 
 
 def default_feeds():
-    return [
-        "http://feeds.libsyn.com/60664",  # Ask a spaceman
-        "https://omny.fm/shows/daniel-and-jorge-explain-the-universe/playlists/podcast.rss",
-        "https://podcasts.files.bbci.co.uk/b00snr0w.rss",  # Infinite monkey cage
-        "https://thecosmicsavannah.com/feed/podcast/",
-        "https://audioboom.com/channels/5014098.rss",  # Supermassive podcast
-        "https://omny.fm/shows/planetary-radio-space-exploration-astronomy-and-sc/playlists/podcast.rss",
-        "https://www.nasa.gov/feeds/podcasts/curious-universe",
-        "https://www.nasa.gov/feeds/podcasts/gravity-assist",
-        "https://rss.art19.com/sean-carrolls-mindscape",
-        "http://titaniumphysics.libsyn.com/rss",
-        "https://www.spreaker.com/show/2458531/episodes/feed",  # Spacetime pod
-        "https://www.abc.net.au/feeds/8294152/podcast.xml",  # Cosmic vertigo
-        "https://astronomycast.libsyn.com/rss",
-        "https://feed.podbean.com/conversationsattheperimeter/feed.xml",
-        "https://feeds.fireside.fm/universetoday/rss",
-        "https://feeds.soundcloud.com/users/soundcloud:users:210527670/sounds.rss",  # Interplanetary
-        "https://stars.library.ucf.edu/walkaboutthegalaxy/all.rss",  # Walkabout the galaxy
-        "https://podcasts.files.bbci.co.uk/b015sqc7.rss",  # The life scientific
-    ]
+    return {
+        "science": [
+            "http://feeds.libsyn.com/60664",  # Ask a spaceman
+            "https://omny.fm/shows/daniel-and-jorge-explain-the-universe/playlists/podcast.rss",
+            "https://podcasts.files.bbci.co.uk/b00snr0w.rss",  # Infinite monkey cage
+            "https://thecosmicsavannah.com/feed/podcast/",
+            "https://audioboom.com/channels/5014098.rss",  # Supermassive podcast
+            "https://omny.fm/shows/planetary-radio-space-exploration-astronomy-and-sc/playlists/podcast.rss",
+            "https://www.nasa.gov/feeds/podcasts/curious-universe",
+            "https://www.nasa.gov/feeds/podcasts/gravity-assist",
+            "https://rss.art19.com/sean-carrolls-mindscape",
+            "http://titaniumphysics.libsyn.com/rss",
+            "https://www.spreaker.com/show/2458531/episodes/feed",  # Spacetime pod
+            "https://www.abc.net.au/feeds/8294152/podcast.xml",  # Cosmic vertigo
+            "https://astronomycast.libsyn.com/rss",
+            "https://feed.podbean.com/conversationsattheperimeter/feed.xml",
+            "https://feeds.fireside.fm/universetoday/rss",
+            "https://feeds.soundcloud.com/users/soundcloud:users:210527670/sounds.rss",  # Interplanetary
+            "https://stars.library.ucf.edu/walkaboutthegalaxy/all.rss",  # Walkabout the galaxy
+            "https://podcasts.files.bbci.co.uk/b015sqc7.rss",  # The life scientific
+        ],
+        "history": {
+            "https://www.spreaker.com/show/5645402/episodes/feed",  # shite talk
+            "http://rss.acast.com/irishhistory"
+        },
+        "management": {
+            "http://feeds.harvardbusiness.org/harvardbusiness/ideacast",
+            "http://feeds.harvardbusiness.org/harvardbusiness/coaching-real-leaders",
+            "https://feeds.feedburner.com/harvardbusiness/on-leadership"
+        }
+    }
 
 
 def chunk(_list, size):
     for i in range(0, len(_list), size):
-        yield _list[i:i+size]
+        yield _list[i:i + size]
+
+
+def get_episode_dict(podcast_metadata, episode_data, transcript: str, collection: str):
+    episode_dict = None
+
+    _id = get_hash(transcript)
+
+    episode_audio_link = [d["href"] for d in episode_data.links if d["rel"] == "enclosure"]
+    if episode_audio_link and len(episode_audio_link) > 0:
+        episode_audio_link = episode_audio_link[0]
+    else:
+        logger.error(f"Skipping episode because it has no MP3")
+        return episode_dict
+
+    try:
+        podcast_title = podcast_metadata.title
+        podcast_link = getattr(podcast_metadata, "link", None)
+        podcast_language = getattr(podcast_metadata, "language", None)
+        podcast_copyright = getattr(podcast_metadata, "rights", None)
+        podcast_author = getattr(podcast_metadata, "author", None)
+        all_tags = []
+
+        podcast_tags = getattr(podcast_metadata, "tags", None)
+        if podcast_tags:
+            all_tags += [d["term"] for d in podcast_tags]
+
+        podcast_image = getattr(podcast_metadata, "image", None)
+        if podcast_image:
+            podcast_image = podcast_image.href
+
+        podcast_type = getattr(podcast_metadata, "itunes_type", None)
+
+        episode_title = episode_data.title
+
+        episode_published_on = time.strftime('%Y-%m-%d', episode_data.published_parsed)
+        episode_web_link = getattr(episode_data, "link", None)
+
+        episode_image = getattr(episode_data, "image", None)
+        if episode_image:
+            episode_image = getattr(episode_image, "href", None)
+
+        episode_summary = getattr(episode_data, "summary", None)
+        episode_subtitle = getattr(episode_data, "subtitle", None)
+        episode_authors = getattr(episode_data, "authors", None)
+        episode_number = getattr(episode_data, "itunes_episode", None)
+        episode_season = getattr(episode_data, "itunes_season", None)
+        episode_type = getattr(episode_data, "itunes_episodetype", None)
+
+        episode_keywords = getattr(episode_data, "itunes_keywords", None)
+        if episode_keywords:
+            all_tags += episode_keywords
+
+        episode_duration = getattr(episode_data, "itunes_duration", None)
+        if episode_duration and ":" in episode_duration:
+            episode_duration = time_to_seconds(episode_duration)
+
+        episode_tags = getattr(episode_data, "tags", None)
+        if episode_tags:
+            all_tags += [d["term"] for d in episode_tags]
+
+        all_tags = [tag.lower() for tag in all_tags]
+        all_tags = list(dict.fromkeys(all_tags))
+
+        episode_dict = {
+            "_id": _id,
+            "_index": "podcasts",
+            "podcast_collection": collection,
+            "podcast_title": podcast_title,
+            "podcast_link": podcast_link,
+            "podcast_language": podcast_language,
+            "podcast_copyright": podcast_copyright,
+            "podcast_author": podcast_author,
+            "podcast_image": podcast_image,
+            "podcast_type": podcast_type,
+            "episode_title": episode_title,
+            "all_tags": all_tags,
+            "episode_published_on": episode_published_on,
+            "episode_audio_link": episode_audio_link,
+            "episode_web_link": episode_web_link,
+            "episode_image": episode_image,
+            "episode_summary": episode_summary,
+            "episode_subtitle": episode_subtitle,
+            "episode_authors": episode_authors,
+            "episode_number": episode_number,
+            "episode_season": episode_season,
+            "episode_type": episode_type,
+            "episode_duration": episode_duration,
+            "episode_transcript": transcript
+        }
+
+        return episode_dict
+
+    except AttributeError as e:
+        logger.error(f"Error getting podcast metadata")
+        logger.error(e)
+        return None
+
+
+def initialise_logging(_logger, _verbose: bool):
+    handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    _logger.addHandler(handler)
+
+    if _verbose:
+        _logger.level = logging.DEBUG
+        handler.setLevel(logging.DEBUG)
+    else:
+        _logger.level = logging.INFO
+        handler.setLevel(logging.INFO)
